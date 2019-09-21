1fbf8edaeab100c4cdac59f2e804ed7782355201
Elizabeth Kipp <liz@elizabethkipp.com>
2019-09-21 17:16:48 -0500

    Removed semicolons form objects in repository array

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,31 @@
 // Add three keys to each Pokémon object
 var repository= [
   {
-    name: 'Bulbasaur';
-    height: 7;
+    name: 'Bulbasaur',
+    height: 7,
     types: [
       'Grass',
       'Monster',
     ]
   },
   {
-    name: 'Charmeleon';
-    height: 1.1;
+    name: 'Charmeleon',
+    height: 1.1,
     types: [
       'Monster' ,
       'Dragon'
     ]
   },
   {
-    name: 'Squirtle';
-    height: 0.5;
+    name: 'Squirtle',
+    height: 0.5,
     types: [
       'Monter',
       'Water1'
     ]
   }
 ];
+
+for (var i = 0; i < repository.length; i++) {
+  document.write(repository.name.height.types);
+}
