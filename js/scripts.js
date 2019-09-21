68b5a3a07965dcbde8436e82b9dfefecfb2f3241
Elizabeth Kipp <liz@elizabethkipp.com>
2019-09-21 14:29:27 -0500

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+var size = 100;
+var doubleSize = size * 2;
+var minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
