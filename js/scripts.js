d0f4a4b8d15425e063b35ba99c5683128c0de07f
Elizabeth Kipp <liz@elizabethkipp.com>
2019-09-22 15:06:15 -0500

    Added Bootstrap tanble and wow that's big to tallest poke

--- js/scripts.js
+++ js/scripts.js
@@ -3,29 +3,23 @@ var repository= [
   {
     name: 'Bulbasaur',
     height: 7,
-    types: [
-      'Grass',
-      'Monster',
-    ]
+    types: [ 'Grass', 'Monster']
   },
   {
     name: 'Charmeleon',
     height: 1.1,
-    types: [
-      'Monster' ,
-      'Dragon'
-    ]
+    types: [ 'Monster', 'Dragon']
   },
   {
     name: 'Squirtle',
     height: 0.5,
-    types: [
-      'Monter',
-      'Water1'
-    ]
+    types: [ 'Monster', 'Water 1' ]
   }
 ];
 
 for (var i = 0; i < repository.length; i++) {
-  document.write(repository.name.height.types);
+  document.write('<br>' + repository[i].name  + ' ' +  'Height: ' + repository[i].height);
+  if(repository[i].height > 2) {
+    document.write(' - Wow, that\'s big!');
+  }
 }
