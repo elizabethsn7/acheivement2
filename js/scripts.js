55748321bcf1c63ced73ec4b4438a9a509ba2336
Elizabeth Kipp <liz@elizabethkipp.com>
2019-09-23 10:55:57 -0500

    Added > to determine biggest pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,27 @@
 // Add three keys to each Pokémon object
-var repository= [
-  {
+var repository = [{
     name: 'Bulbasaur',
     height: 7,
-    types: [ 'Grass', 'Monster']
+    types: ['Grass', 'Monster']
   },
   {
     name: 'Charmeleon',
     height: 1.1,
-    types: [ 'Monster', 'Dragon']
+    types: ['Monster', 'Dragon']
   },
   {
     name: 'Squirtle',
     height: 0.5,
-    types: [ 'Monster', 'Water 1' ]
+    types: ['Monster', 'Water 1']
   }
 ];
 
 for (var i = 0; i < repository.length; i++) {
-  var pokemonHeight = repository[i].height;
-  var tallestPokemon = Math.max(pokemonHeight);
+  var pokeName = repository[i].name;
+  var pokeHeight = repository[i].height;
+  document.write('<br>' + pokeName + ' ' + ' (' +  'Height : ' + pokeHeight +  ')');
 
-  document.write('<br>' + repository[i].name  + ' ' +  'Height: ' + repository[i].height);
-
-  if (tallestPokemon) {
-    document.write('-Wow, that\'s tall!');
+  if(pokeHeight> 2) {
+    document.write(' - Wow, that\'s big!');
   }
 }
-
-
-
-
-
- // I have to list all of the pokemons. THen I want to find the tallestPokemon and once I find it, I want to add a string to it. I think I need to use math.max and then the .push method.
-
-
-// var tallestPokemon =
-//   if(tallestPokemon) {
-//     repository[i].height.push(' -Wow, that\'s big!')
-//   }
