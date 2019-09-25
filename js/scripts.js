2f1da13e383dcd046e24107686f3c4905a406c58
Elizabeth Kipp <liz@elizabethkipp.com>
2019-09-25 13:40:46 -0500

    Created IIFE around repo array with public functions getAll and add

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,56 @@
-// Add three keys to each Pokémon object
-var repository = [{
-    name: 'Bulbasaur',
-    height: 7,
-    types: ['Grass', 'Monster']
-  },
-  {
-    name: 'Charmeleon',
-    height: 1.1,
-    types: ['Monster', 'Dragon']
-  },
-  {
-    name: 'Squirtle',
-    height: 0.5,
-    types: ['Monster', 'Water 1']
-  }
-];
+var pokemonRepository = (function() {
+  var repository = [{
+      name: 'Bulbasaur',
+      height: 7,
+      types: ['Grass', 'Monster']
+    },
+    {
+      name: 'Charmeleon',
+      height: 1.1,
+      types: ['Monster', 'Dragon']
+    },
+    {
+      name: 'Squirtle',
+      height: 0.5,
+      types: ['Monster', 'Water 1']
+    }
+  ]; // Closes repository[]
 
-repository.forEach(function(properties) {
-  console.log(properties);
+  // Make sure both getAll and add are defined separatetly
+  //with the function keyword.
+  function add(pokemon) {
+    // Check if the typeof parameter is an object
+    if (typeof pokemon === 'object') {
+      repository.push(pokemon);
+    }
+  }
+  function getAll() {
+    return repository;
+  }
+  // Return an object with public funtions assigned as keys
+  return {
+    add: add,
+    getAll: getAll
+  };
+
+// Attempting Bonus ro no Avail...
+function filteredNames() {
+  var names = repository.name;
+  return filteredNames === names.filter('Squirtle')
+}
+console.log(filteredNames);
+})();
+
+// Update to use one of the two functions returned by the IIFE in order to retrieve the repository array.
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({
+  name: 'Pikachu',
+  height: 2,
+  types: ['Water', "Air"]
+});
+console.log(pokemonRepository.getAll());
+
+//  Validate whether all Object.keys()of the parameter are equal to the specific keys
+Object.keys(pokemonRepository).forEach(function(property) {
+console.log(pokemonRepository[property]);
 });
