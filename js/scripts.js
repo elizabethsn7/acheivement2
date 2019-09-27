fb011d258839f208d21848f2d6dd0b085250efb7
Elizabeth Kipp <liz@elizabethkipp.com>
2019-09-26 22:14:50 -0500

    Created button and list in js with querySelectors

--- js/scripts.js
+++ js/scripts.js
@@ -29,29 +29,30 @@ var pokemonRepository = (function() {
     return repository;
   }
 
-// Bonus Task using .filter () method
-var filterName = repository.filter(function(names) {
-  return names.name === 'Squirtle'
-});
-console.log(filterName);
-
+  function addListItem(pokemon) {
+    var button = document.createElement('button');
+    button.innerText = pokemon;
+    button.classList.add('pokemonButton');
+    $element.appendChild(button);
+  }
   // Return an object with public funtions assigned as keys
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem
   };
 })();
 
-// Update to use one of the two functions returned by the IIFE in order to retrieve the repository array.
-console.log(pokemonRepository.getAll());
 pokemonRepository.add({
   name: 'Pikachu',
   height: 2,
   types: ['Water', "Air"]
 });
-console.log(pokemonRepository.getAll());
 
-//  Validate whether all Object.keys()of the parameter are equal to the specific keys
-Object.keys(pokemonRepository).forEach(function(property) {
-  console.log(pokemonRepository[property]);
-});
+var $element = document.querySelector('ul');
+
+pokemonRepository.getAll().forEach(function(property) {
+  var listItem = document.createElement('li');
+  $element.appendChild(listItem);
+  pokemonRepository.addListItem(property.name);
+})
