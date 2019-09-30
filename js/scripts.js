c49347d7ba45e6eae60eadc6de6d39c5db72af90
Elizabeth Kipp <liz@elizabethkipp.com>
2019-09-30 09:45:37 -0500

    added polyfills, api to fetch and load data

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,6 @@
 var pokemonRepository = (function() {
-  var repository = [{
-      name: 'Bulbasaur',
-      height: 7,
-      types: ['Grass', 'Monster']
-    },
-    {
-      name: 'Charmeleon',
-      height: 1.1,
-      types: ['Monster', 'Dragon']
-    },
-    {
-      name: 'Squirtle',
-      height: 0.5,
-      types: ['Monster', 'Water 1']
-    }
-  ]; // Closes repository[]
+  var repository = [];
+  var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   // Make sure both getAll and add are defined separatetly
   //with the function keyword.
@@ -30,30 +16,62 @@ var pokemonRepository = (function() {
   }
 
   function showDetails(pokemon) {
-    console.log(pokemon);
+    pokemonRepository.loadDetails(pokemon).then(function() {
+      console.log(pokemon);
+    });
   }
 
   function addListItem(pokemon) {
+    var $element = document.querySelector('ul');
+    var listItem = document.createElement('li');
     var button = document.createElement('button');
     button.innerText = pokemon;
     button.classList.add('pokemonButton');
+    $element.appendChild(listItem);
     $element.appendChild(button);
     button.addEventListener('click', function(showDetails) {
       console.log(pokemon);
     });
   }
 
+  function loadList() {
+    return fetch(apiUrl).then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      json.results.forEach(function(item) {
+        var pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function(e) {
+      console.error(e);
+    })
+  }
+
+  function loadDetails(item) {
+    var url = item.detailsUrl;
+    return fetch(url).then(function(response) {
+      return response.json();
+    }).then(function(details) {
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = Object.keys(details.types);
+    }).catch(function(e) {
+      console.error(e);
+    });
+  }
   // Return an object with public funtions assigned as keys
   return {
     add: add,
     getAll: getAll,
+    loadList: loadList,
+    loadDetails: loadDetails,
     addListItem: addListItem
   };
 })();
 
-var $element = document.querySelector('ul');
-pokemonRepository.getAll().forEach(function(property) {
-  var listItem = document.createElement('li');
-  $element.appendChild(listItem);
-  pokemonRepository.addListItem(property.name);
-})
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(pokemonRepository.addListItem);
+});
