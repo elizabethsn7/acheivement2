505d46790fa5c89931442483fd34741873005a6a
Elizabeth Kipp <liz@elizabethkipp.com>
2019-10-03 16:18:35 -0500

    Title of Poke appends to Modal button

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var pokemonRepository = (function () {
-  var repository = [ ];
+var pokemonRepository = (function() {
+  var repository = [];
   var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=5';
 
   function add(pokemon) {
@@ -11,10 +11,12 @@ var pokemonRepository = (function () {
   }
 
   function showDetails(pokemon) {
-    pokemonRepository.loadDetails(pokemon).then(function () {
-      console.log(pokemon);
-    });
-  }
+    pokemonRepository.loadDetails(pokemon).then(function() {
+      document.querySelector('#show-modal').addEventListener('click', () => {
+        showModal(pokemon.name);
+      });
+    }); // ** closes (Promise?) ** //
+  } //  ***** Closes showDetails ***** //
 
   function addListItem(pokemon) {
     var ulElement = document.querySelector('.pokemonList');
@@ -24,38 +26,38 @@ var pokemonRepository = (function () {
     button.classList.add('pokemonButton');
     listItem.appendChild(button);
     ulElement.appendChild(listItem);
-// **** Add Event listener  **** //
+    // **** Add Event listener  **** //
     button.addEventListener('click', function() {
       showDetails(pokemon);
     });
   }
 
-function loadList() {
-  return fetch(apiUrl).then(function (response) {
-    return response.json();
-  }).then(function (json) {
-    json.results.forEach(function (item) {
-      var pokemon = {
-        name: item.name,
-        detailsUrl: item.url
-      };
-      add(pokemon);
-    });
-  }).catch(function (e) {
-    console.error(e);
-  })
-}
+  function loadList() {
+    return fetch(apiUrl).then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      json.results.forEach(function(item) {
+        var pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function(e) {
+      console.error(e);
+    })
+  }
 
   function loadDetails(pokemon) {
     var url = pokemon.detailsUrl;
-    return fetch(url).then(function (response) {
+    return fetch(url).then(function(response) {
       return response.json();
-    }).then(function (details) {
+    }).then(function(details) {
       // Now we add the details to the item
       pokemon.imageUrl = details.sprites.front_default;
       pokemon.height = details.height;
       pokemon.types = Object.keys(details.types);
-    }).catch(function (e) {
+    }).catch(function(e) {
       console.error(e);
     });
   }
@@ -69,7 +71,51 @@ function loadList() {
   }; // *** Closes Return
 })();
 
+
 pokemonRepository.loadList().then(function() {
   //Now the data is loaded!
   pokemonRepository.getAll().forEach(pokemonRepository.addListItem);
 });
+
+function showModal(title, text) {
+  var $modalContainer = document.querySelector('#modal-container');
+  //Clear existing modal conent
+  $modalContainer.innerHTML = ' ';
+  var modal = document.createElement('div');
+  modal.classList.add('modal');
+
+  // Add the new modal content
+  var closeButtonElement = document.createElement('button');
+  closeButtonElement.classList.add('modal-close');
+  closeButtonElement.innerText = 'Close';
+  closeButtonElement.addEventListener('click', hideModal);
+
+  var titleElement = document.createElement('h1');
+  titleElement.innerText = title;
+
+  var contentElement = document.createElement('p');
+  contentElement.innerText = text;
+
+  modal.appendChild(closeButtonElement);
+  modal.appendChild(titleElement);
+  modal.appendChild(contentElement);
+  $modalContainer.appendChild(modal);
+
+  $modalContainer.classList.add('is-visible');
+} // *** Closes showModal *** //
+
+function hideModal() {
+  var $modalContainer = document.querySelector('#modal-container');
+  $modalContainer.classList.remove('is-visible');
+}
+
+document.querySelector('#show-modal').addEventListener('click', () => {
+  showModal('Modal Title', 'This is the modal content.');
+});
+
+window.addEventListener('keydown', (e) => {
+  var $modalContainer = document.querySelector('#modal-container');
+  if (e.key === 'Escape' && $modalContainer.classList.contains('is-visible')) {
+    hideModal();
+  }
+})
